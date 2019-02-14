c0c1ea188ae85c4ee2fc22401244cfb80f6d24f1
Evandro Carreira <evandro.carreira@gmail.com>
2019-02-14 18:37:45 -0200

    Explorando Flex

--- App.js
+++ App.js
@@ -1,54 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 class NavBar extends React.Component {
   render() {
     return <View style={styles.menubar}>
-      <View style={styles.flexBlock}>Voltar</View>
-      <View style={styles.flexGrow}>Logo</View>
-      <View style={styles.flexBlock}>Menu</View>
+      <View style={styles.flexBlock}><Text>Voltar</Text></View>
+      <View style={styles.flexGrow}><Text>Logo</Text></View>
+      <View style={styles.flexBlock}><Text>Menu</Text></View>
     </View>
   }
 }
+
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+         <NavBar/>
+         <ScrollView style={styles.content}>
+          <View style={styles.bloco500}>
+            <Text>Bloco</Text>
+          </View>
+          <View style={styles.bloco500}>
+            <Text>Mais um bloco</Text>
+          </View>
+         </ScrollView>
+       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0ff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  content :{
+    alignSelf: 'stretch',
+  },
+  bloco500: {
+    flex: 0,
+    flexGrow: 0,
+    height: 350,
     alignItems: 'center',
     justifyContent: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: '#ddd',
+    margin: 4
   },
   menubar: {
-    flex: 1,
-    width: 500,
-    height: 100,
-    borderBottom: '1px solid black',
-    alignItems: 'flex-start',
-    padding: 20,
+    flexDirection: 'row',
+    flex: 0,
+    height: 60,
+    flexGrow: 0,
+    backgroundColor: 'yellow',
   },
   flexBlock : {
     flex: 1,
-    width: 100,
-    height: 100,
-    flexGrow: 0,
+    flexGrow: 1,
     justifyContent: 'center',
+    backgroundColor: 'green',
     alignItems: 'center',
   },
   flexGrow : {
     flex: 1,
-    width: 100,
-    height: 100,
-    flexGrow: 0,
+    flexGrow: 3,
     justifyContent: 'center',
+    backgroundColor: 'red',
     alignItems: 'center',
   }
 });
